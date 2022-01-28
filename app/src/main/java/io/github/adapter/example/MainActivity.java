ada2ff2509277e71b7cff7832ef88a7fefd2b5e6
wong1988 <wong1988@petalmail.com>
2022-01-28 10:18:42 +0800

    Update MainActivity.java

--- app/src/main/java/io/github/adapter/example/MainActivity.java
+++ app/src/main/java/io/github/adapter/example/MainActivity.java
@@ -1,6 +1,8 @@
 package io.github.adapter.example;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void linearManager_vertical(View view) {
+        startActivity(new Intent(this, LinearVerticalActivity.class));
+    }
+
+    public void linearManager_horizontal(View view) {
+        startActivity(new Intent(this, LinearHorizontalActivity.class));
     }
 }
