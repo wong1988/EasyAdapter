e5c65364a0e93bdf63d4d625720fda6bfac645f5
wong1988 <94215397+wong1988@users.noreply.github.com>
2022-07-11 14:24:37 +0800

    Update LinearVerticalAdapter.java

--- app/src/main/java/io/github/adapter/example/adapter/LinearVerticalAdapter.java
+++ app/src/main/java/io/github/adapter/example/adapter/LinearVerticalAdapter.java
@@ -1,8 +1,6 @@
 package io.github.adapter.example.adapter;
 
 import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.Arrays;
