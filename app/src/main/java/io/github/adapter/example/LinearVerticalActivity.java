2b6c7cd41c69f2b4f355b49047853a0581e2a30d
wong1988 <94215397+wong1988@users.noreply.github.com>
2022-07-11 14:00:05 +0800

    Update LinearVerticalActivity.java

--- app/src/main/java/io/github/adapter/example/LinearVerticalActivity.java
+++ app/src/main/java/io/github/adapter/example/LinearVerticalActivity.java
@@ -2,7 +2,10 @@ package io.github.adapter.example;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,22 +16,80 @@ import io.github.wong1988.adapter.divider.LinearLayoutManagerDivider;
 
 public class LinearVerticalActivity extends AppCompatActivity {
 
+    private LinearVerticalAdapter adapter;
+    private RecyclerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_vertical);
 
-        RecyclerView rv = findViewById(R.id.rv);
+        rv = findViewById(R.id.rv);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
         rv.setLayoutManager(linearLayoutManager);
+        rv.addItemDecoration(LinearLayoutManagerDivider.getVerticalDivider());
 
-//        rv.addItemDecoration(LinearLayoutManagerDivider.getVerticalDivider());
-        rv.addItemDecoration(LinearLayoutManagerDivider.getVerticalDivider(Color.parseColor("#000000"), 20));
-//        rv.addItemDecoration(LinearLayoutManagerDivider.getVerticalDivider(Color.parseColor("#000000"), 20, 10, 20));
-
-        LinearVerticalAdapter adapter = new LinearVerticalAdapter(this);
+        adapter = new LinearVerticalAdapter(this);
         rv.setAdapter(adapter);
         adapter.setLoadState(LoadState.LOAD_END);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(1, 0, 1, "添加头布局");
+        menu.add(1, 1, 2, "添加脚布局");
+        menu.add(1, 2, 3, "清空头布局");
+        menu.add(1, 3, 4, "删除头布局1");
+        menu.add(1, 4, 5, "清空脚布局");
+        menu.add(1, 5, 6, "删除脚布局1");
+        menu.add(1, 6, 6, "20高分割线");
+        menu.add(1, 7, 6, "20高 距左10 距右20 分割线");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                adapter.addHeader(LinearVerticalActivity.this);
+                rv.scrollToPosition(0);
+                break;
+            case 1:
+                rv.scrollToPosition(adapter.getItemCount() - 1);
+                adapter.addFooter();
+                break;
+            case 2:
+                rv.scrollToPosition(0);
+                adapter.removeAllHeaders();
+                break;
+            case 3:
+                rv.scrollToPosition(0);
+                adapter.removeHeader(0);
+                break;
+            case 4:
+                rv.scrollToPosition(adapter.getItemCount() - 1);
+                adapter.removeAllFooters();
+                break;
+            case 5:
+                rv.scrollToPosition(adapter.getItemCount() - 1);
+                adapter.removeFooter(0);
+                break;
+            case 6:
+                int itemDecorationCount = rv.getItemDecorationCount();
+                for (int i = 0; i < itemDecorationCount; i++) {
+                    rv.removeItemDecorationAt(i);
+                }
+                rv.addItemDecoration(LinearLayoutManagerDivider.getVerticalDivider(Color.parseColor("#000000"), 20));
+                break;
+            case 7:
+                int itemDecorationCount2 = rv.getItemDecorationCount();
+                for (int i = 0; i < itemDecorationCount2; i++) {
+                    rv.removeItemDecorationAt(i);
+                }
+                rv.addItemDecoration(LinearLayoutManagerDivider.getVerticalDivider(Color.parseColor("#000000"), 20, 10, 20));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
