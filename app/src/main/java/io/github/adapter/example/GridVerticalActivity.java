c90f9e5d6b5a7eaf9553fedf43999adcca244b9a
wong1988 <94215397+wong1988@users.noreply.github.com>
2022-07-11 16:01:12 +0800

    Update GridVerticalActivity.java

--- app/src/main/java/io/github/adapter/example/GridVerticalActivity.java
+++ app/src/main/java/io/github/adapter/example/GridVerticalActivity.java
@@ -2,7 +2,10 @@ package io.github.adapter.example;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,19 +17,69 @@ import io.github.wong1988.adapter.divider.GridLayoutManagerDivider;
 
 public class GridVerticalActivity extends AppCompatActivity {
 
+    private LinearVerticalAdapter adapter;
+    private RecyclerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_vertical);
 
-        RecyclerView rv = findViewById(R.id.rv);
+        rv = findViewById(R.id.rv);
         LinearLayoutManager linearLayoutManager = new GridLayoutManager(this, 3);
         linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
         rv.setLayoutManager(linearLayoutManager);
-        rv.addItemDecoration(GridLayoutManagerDivider.getVerticalDivider(Color.parseColor("#000000"), 20));
+        rv.addItemDecoration(GridLayoutManagerDivider.getVerticalDivider());
 
-        LinearVerticalAdapter adapter = new LinearVerticalAdapter(this);
+        adapter = new LinearVerticalAdapter(this);
         rv.setAdapter(adapter);
         adapter.setLoadState(LoadState.LOAD_ERROR);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(1, 0, 1, "添加头布局");
+        menu.add(1, 1, 2, "添加脚布局");
+        menu.add(1, 2, 3, "清空头布局");
+        menu.add(1, 3, 4, "删除头布局1");
+        menu.add(1, 4, 5, "清空脚布局");
+        menu.add(1, 5, 6, "删除脚布局1");
+        menu.add(1, 6, 6, "20高分割线");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                adapter.addHeader();
+                rv.scrollToPosition(0);
+                break;
+            case 1:
+                rv.scrollToPosition(adapter.getItemCount() - 1);
+                adapter.addFooter();
+                break;
+            case 2:
+                rv.scrollToPosition(0);
+                adapter.removeAllHeaders();
+                break;
+            case 3:
+                rv.scrollToPosition(0);
+                adapter.removeHeader(0);
+                break;
+            case 4:
+                rv.scrollToPosition(adapter.getItemCount() - 1);
+                adapter.removeAllFooters();
+                break;
+            case 5:
+                rv.scrollToPosition(adapter.getItemCount() - 1);
+                adapter.removeFooter(0);
+                break;
+            case 6:
+                adapter.removeAllItemDecoration();
+                rv.addItemDecoration(GridLayoutManagerDivider.getVerticalDivider(Color.parseColor("#000000"), 20));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
