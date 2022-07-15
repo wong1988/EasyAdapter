1bdd5357e04467accb71217026aa7335baf216f7
wong1988 <94215397+wong1988@users.noreply.github.com>
2022-07-15 10:09:36 +0800

    Update GridVerticalActivity.java

--- app/src/main/java/io/github/adapter/example/GridVerticalActivity.java
+++ app/src/main/java/io/github/adapter/example/GridVerticalActivity.java
@@ -39,15 +39,15 @@ public class GridVerticalActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(1, 0, 1, "添加头布局");
-        menu.add(1, 1, 2, "添加脚布局");
+        menu.add(1, 0, 1, "添加头布局[垂直方向]");
+        menu.add(1, 1, 2, "添加脚布局[垂直方向]");
         menu.add(1, 2, 3, "清空头布局");
-        menu.add(1, 3, 4, "删除头布局1");
+        menu.add(1, 3, 4, "删除头布局中的第一个view");
         menu.add(1, 4, 5, "清空脚布局");
-        menu.add(1, 5, 6, "删除脚布局1");
+        menu.add(1, 5, 6, "删除脚布局中的第一个view");
         menu.add(1, 6, 7, "20高分割线");
-        menu.add(1, 7, 8, "设置头布局的宽高");
-        menu.add(1, 8, 9, "设置脚布局的宽高");
+        menu.add(1, 7, 8, "设置头布局的宽高[WRAP_CONTENT]");
+        menu.add(1, 8, 9, "设置脚布局的宽高[WRAP_CONTENT]");
         return super.onCreateOptionsMenu(menu);
     }
 
